0704514250cd662e0af98cb7d87491d0a76ef15d
Pedro Clain <pedroclain20@gmail.com>
2023-12-29 18:15:26 -0300

    add purchase form

--- front_end/src/app/pages/gamedetails/game-details.component.ts
+++ front_end/src/app/pages/gamedetails/game-details.component.ts
@@ -1,9 +1,16 @@
 import { Component, Input } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { LoadingService } from '../../services/loading.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Game } from '../../models/game.model';
 import { GameService } from '../../services/game.service';
+import { AlertService } from '../../services/alert.service';
 
 @Component({
   selector: 'app-game-details',
@@ -13,17 +20,25 @@ import { GameService } from '../../services/game.service';
   styleUrl: './game-details.component.css',
 })
 export class GameDetails {
-  invalidFields: Set<string> = new Set();
   game: Game | null = null;
 
-  contactForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new  FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    phone: new FormControl(''),
+  purchaseForm = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: [null, [Validators.min(999999999), Validators.max(99999999999)]],
+    cardForm: this.formBuilder.group({
+      number: [null, [Validators.required]],
+      cvv: [null, [Validators.required, Validators.min(1), Validators.max(999)]],
+      validation: [null, [Validators.required]],
+    }),
   });
 
-  constructor(private gameService: GameService, private loadingService: LoadingService) {}
+  constructor(
+    private gameService: GameService,
+    private loadingService: LoadingService,
+    private alertService: AlertService,
+    private formBuilder: FormBuilder
+  ) {}
 
   @Input()
   set id(id: number) {
@@ -34,18 +49,15 @@ export class GameDetails {
         this.loadingService.closeLoading();
       },
       error: (err: HttpErrorResponse) => {
-        alert('error' + err.status)
-      }
+        alert('error' + err.status);
+      },
     });
   }
 
   submitContactForm() {
-    alert('Enviando formulário..');
-    this.contactForm.patchValue({
-      email: '',
-      firstName: '',
-      lastName: '',
-      phone: ''
+    this.alertService.openAlert({
+      content: "This title will be purchase soon...",
+      type: "success"
     })
   }
 }
